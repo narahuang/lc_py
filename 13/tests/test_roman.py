3e00bd13ee077ee589a8ead5389c924944e53db4
Nara Huang <nhuang@milwaukeeelectronics.com>
2026-01-12 12:03:44 +0800

    Refactor: Refine pytest with parameters

--- 13/tests/test_roman.py
+++ 13/tests/test_roman.py
@@ -1,23 +1,15 @@
+import pytest
 from src.roman import Solution
 sol = Solution()
 
-def test_3():
-    assert sol.romanToInt('III') == 3
-    
-def test_4():
-    assert sol.romanToInt('IV') == 4
-
-def test_9():
-    assert sol.romanToInt('IX') == 9
-
-def test_58():
-    assert sol.romanToInt('LVIII') == 58
-
-def test_1994():
-    assert sol.romanToInt('MCMXCIV') == 1994
-
-def test_1999():
-    assert sol.romanToInt('MCMXCIX') == 1999
-    
-def test_2026():
-    assert sol.romanToInt('MMXXVI') == 2026
+@pytest.mark.parametrize("s, expected", [
+    ('III', 3),
+    ('IV', 4),
+    ('IX', 9),
+    ('LVIII', 58),
+    ('MCMXCIV', 1994),
+    ('MCMXCIX', 1999),
+    ('MMXXVI', 2026)
+])
+def test_romanToInt(s, expected):
+    assert sol.romanToInt(s) == expected
